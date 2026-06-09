fb0c820702d44e33573847d482b9e3e42ad447b3
taivv18 <taivv18@gmail.com>
2026-06-09 11:17:43 +0700

    chore: init exercise

---
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0,
+            sum = 0;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+
+            if (number > 0) {
+                ++count;
+                sum += number;
+            }
+        }
+        if (count > 0) {
+            double result = (1.0 * sum) / count;
+            System.out.println(result);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+    }
+}
