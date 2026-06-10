2837baf52fe1dee7bb9541a8e47976745cafd09e
taivv18 <taivv18@gmail.com>
2026-06-11 00:08:18 +0700

    feat: completed Cubes

--- mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Cubes {
@@ -6,5 +5,16 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("end")) {
+                break;
+            }
+
+            int num = Integer.valueOf(command);
+            if (num > 0) {
+                System.out.println(num * num * num);
+            }
+        }
     }
 }
