027038714067764a685f0a7554d6c64d5c4e0e62
taivv18 <taivv18@gmail.com>
2026-06-11 00:22:06 +0700

    feat: completed AverageOfPositiveNumbers

--- mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
@@ -6,5 +5,26 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int countPositiveNubmer = 0;
+        int sum = 0;
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("0")) {
+                break;
+            }
+
+            int num = Integer.valueOf(command);
+            if (num > 0) {
+                sum += num;
+                ++countPositiveNubmer;
+            }
+        }
+
+        if (countPositiveNubmer == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((1.0 * sum) / countPositiveNubmer);
+        }
     }
 }
