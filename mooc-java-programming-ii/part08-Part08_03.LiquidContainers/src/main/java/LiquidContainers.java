c08cdd16870f4a03f4f86aa52ce8b434512ac7a2
taivv18 <taivv18@gmail.com>
2026-06-11 11:07:43 +0700

    feat: demo function add and specifics function

--- mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class LiquidContainers {
@@ -6,16 +5,29 @@ public class LiquidContainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
+        int first = 0;
+        int second = 0;
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
 
+            // user input
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            String[] parts = input.split(" ");
+            if (parts[0].equals("quit")) {
                 break;
             }
 
+            // add amount adds the amount of liquid specified by the parameter to the first container. The inserted amount must be specified as an integer. The container can't hold more than a hundred liters and everything added past that will go to waste.
+
+            int amount = Integer.valueOf(parts[1]);
+            if (input.equals("add")) {
+                if (amount >= 100) {
+                    first = 100;
+                    amount -= 100;
+                }
+                first += amount;
+            }
         }
     }
-
 }
