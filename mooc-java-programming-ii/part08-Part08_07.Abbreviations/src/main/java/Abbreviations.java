c167c2977184a7e2a52254779a602c20a891830a
taivv18 <taivv18@gmail.com>
2026-06-14 12:09:12 +0700

    chore: skeleton of object

---
+++ mooc-java-programming-ii/part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,16 @@
+public class Abbreviations {
+
+    private String abbreviation;
+    private String explanation;
+
+    public Abbreviations(String abbreviation, String explanation) {
+        this.abbreviation = abbreviation;
+        this.explanation = explanation;
+    }
+
+    public void addAbbreviations(String abbreviation, String explanation) {}
+
+    public boolean hasAbbreviation(String abbreviation) {}
+
+    public String findExplanationFor(String abbreviation) {}
+}
