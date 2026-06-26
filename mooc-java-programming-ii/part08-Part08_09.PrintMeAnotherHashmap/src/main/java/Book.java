bb6ff72e4a418c07ebff43524305c3db7342b20b
taivv18 <taivv18@gmail.com>
2026-06-26 13:32:49 +0700

    feat: completed PrintMeAnotherHashmap

--- mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Book.java
+++ mooc-java-programming-ii/part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Book.java
@@ -1,42 +1,46 @@
-
 public class Book {
 
-    private String name;
-    private String contents;
-    private int publicationYear;
+  private String name;
+  private String contents;
+  private int publicationYear;
 
-    public Book(String name, int publicationYear, String contents) {
-        this.name = name;
-        this.publicationYear = publicationYear;
-        this.contents = contents;
-    }
+  public Book(String name, int publicationYear, String contents) {
+    this.name = name;
+    this.publicationYear = publicationYear;
+    this.contents = contents;
+  }
 
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getPublicationYear() {
-        return this.publicationYear;
-    }
+  public int getPublicationYear() {
+    return this.publicationYear;
+  }
 
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
-    }
+  public void setPublicationYear(int publicationYear) {
+    this.publicationYear = publicationYear;
+  }
 
-    public String getContents() {
-        return this.contents;
-    }
+  public String getContents() {
+    return this.contents;
+  }
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
 
-    public String toString() {
-        return "Name: " + this.name + " (" + this.publicationYear + ")\n"
-                + "Contents: " + this.contents;
-    }
+  public String toString() {
+    return "Name: "
+        + this.name
+        + " ("
+        + this.publicationYear
+        + ")\n"
+        + "Contents: "
+        + this.contents;
+  }
 }
